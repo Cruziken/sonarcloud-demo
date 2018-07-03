42c8f941a8561878a4a25bf201b715ed0b9478a4
Benjamin <bencruz@vols.utk.edu>
2018-07-03 11:43:26 -0400

    Testing commit and issuses raiser SonarQube

--- src/main/java/org/cruziken/sonarclouddemo/SonarcloudDemoApplication.java
+++ src/main/java/org/cruziken/sonarclouddemo/SonarcloudDemoApplication.java
@@ -8,6 +8,6 @@ public class SonarcloudDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonarcloudDemoApplication.class, args);
-		System.out.print("We here now right");
+		System.out.print("We here now right so lets see what changes pop up");
 	}
 }
